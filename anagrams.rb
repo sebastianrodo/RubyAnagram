85c66e03eb3c8d5c277ee382c34077140017111d
sebastianrodo <sebastian.rodriguez@koombea.com>
2021-02-25 14:34:23 -0500

    Add test for the methods get_anagrams and get_anangrams

--- anagrams.rb
+++ anagrams.rb
@@ -1,21 +1,29 @@
 require 'pry'
+
 class Anagram
   WORDS = File.read("words.txt").split
 
-  @anagrams = []
-  @anangrams = []
-
-  def self.sort_list_of_words
-    words_hash = WORDS.each_with_object(Hash.new []) do |word, hash|
-      hash[word.chars.sort] += [word]
-    end
-
-    words_hash.each do |word, matching_words|
-      matching_words.length > 1 ? @anagrams += matching_words : @anangrams += matching_words
+  def get_anagrams
+    sort_list_of_words.select do |word, matching_words|
+      matching_words.length > 1
     end
   end
 
-  def self.find_anagrams_in_the_list(word_to_find)
+  def get_ananagrams
+    sort_list_of_words.select do |word, matching_words|
+      matching_words.length == 1
+    end
+  end
+
+  def search_anagrams(word_to_find)
     WORDS.select { |word| word.chars.sort === word_to_find.chars.sort }
+  end
+
+  private
+
+  def sort_list_of_words
+    WORDS.each_with_object(Hash.new []) do |word, hash|
+      hash[word.chars.sort] += [word]
+    end
   end
 end
--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -1,11 +1,43 @@
 require_relative '../anagrams'
 
-RSpec.describe 'Anagrams' do
-  context "" do
-    it "should return the array of valid words" do
-      valid_words = @a.solve("art")
-      expect(valid_words).to be_kind_of(Array)
-      expect(valid_words).to_not be_empty
+RSpec.describe Anagram do
+  describe 'search anagrams in the list' do
+    subject { instance.search_anagrams(word) }
+
+    let!(:instance) { described_class.new }
+    context 'with anagram found' do
+      let(:word) { 'weird' }
+
+      it 'expect return the anagrams found' do
+        expect(subject).to match_array ["weird", "wider", "wired"]
+      end
+    end
+
+    context 'without anagram found' do
+      let(:word) { 'try' }
+
+      it 'expect return any anagram found' do
+        expect(subject).to match_array([])
+      end
     end
   end
+
+  describe 'get anagrams' do
+    subject { instance.get_anagrams }
+
+    let!(:instance) { described_class.new }
+
+    it { expect(subject).to be_kind_of(Hash) }
+    it { expect(subject.values.first.size).to be > 1 }
+  end
+
+  describe 'get ananagrams' do
+    subject { instance.get_ananagrams }
+
+    let!(:instance) { described_class.new }
+
+    it { expect(subject).to be_kind_of(Hash) }
+    it { expect(subject.values.first.size).to eq(1) }
+  end
+
 end
